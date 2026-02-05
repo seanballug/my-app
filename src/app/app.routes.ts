aad477fdcf64ac69948a69c22becbdcebce01de7
seanballug <20171143@s.ubaguio.edu>
2026-02-05 13:26:03 +0800

    Added home and profile components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './comps/home/home';
+import { LoginComponent} from './comps/login/login';
+import { ProfileComponent } from './comps/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'profile', component: ProfileComponent}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navi } from './comps/navi/navi';
-import { Login } from "./comps/login/login";
+import { LoginComponent} from "./comps/login/login";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navi, Login],
+  imports: [RouterOutlet, Navi, LoginComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('my-app');
 }
